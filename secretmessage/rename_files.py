a5d9035478e4af474ecd94b13c552774d1eda69d
Chris <c187.org@gmail.com>
2017-11-03 13:49:57 -0700

    Rename successful
    The program has successfully renamed the files to get the secret
    message. The program has now been cleaned up. See past version for
    progress.

--- secretmessage/rename_files.py
+++ secretmessage/rename_files.py
@@ -1,7 +1,7 @@
 import os
 def rename_files():
     # (1) get file names from a folder
-    # NOTE Filed directory unique to my Win10 machine 
+    # NOTE File directory unique to my Win10 machine. Please import your directory to run program. 
     file_list = os.listdir(r"C:\Users\c187r\OneDrive\Documents\GitHub\udacity\secretmessage\prank")
     #print(file_list)
     saved_path = os.getcwd()
@@ -9,11 +9,12 @@ def rename_files():
     os.chdir(r"C:\Users\c187r\OneDrive\Documents\GitHub\udacity\secretmessage\prank")
 
     # (2) for each file, rename filename
-    # Udacity Lesson 5, Part 22 Quiz: Find the name of the fuction to rename. That fuction is os.rename
     for file_name in file_list:
+        print("Old Name - "+file_name)
+        print("New Name - "+file_name.translate(None, "0123456789"))
         os.rename(file_name, file_name.translate(None, "0123456789"))
     os.chdir(saved_path)
-        # NOTE Error given. Program is not finding dir
-        # DEBUG Result: Dir is a level down from the dir wanted. It's running in the same dir as the program
-        # DEBUG DONE, Program works
+    
 rename_files()
+
+#NOTE Files on GitHub have been renamed. Unzip prank.zip to get a set of files to rename.
