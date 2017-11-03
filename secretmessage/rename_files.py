3bbdbbbded962a8c07cbe5110213b6915bf1574d
Chris <c187.org@gmail.com>
2017-11-03 13:42:02 -0700

    Debug done, program works, images renamed

--- secretmessage/rename_files.py
+++ secretmessage/rename_files.py
@@ -2,14 +2,18 @@ import os
 def rename_files():
     # (1) get file names from a folder
     # NOTE Filed directory unique to my Win10 machine 
-    #file_list = os.listdir(r"C:\Users\c187r\OneDrive\Documents\GitHub\udacity\secretmessage\prank")
+    file_list = os.listdir(r"C:\Users\c187r\OneDrive\Documents\GitHub\udacity\secretmessage\prank")
     #print(file_list)
     saved_path = os.getcwd()
     print("Current Working Directory is "+saved_path)
+    os.chdir(r"C:\Users\c187r\OneDrive\Documents\GitHub\udacity\secretmessage\prank")
+
     # (2) for each file, rename filename
     # Udacity Lesson 5, Part 22 Quiz: Find the name of the fuction to rename. That fuction is os.rename
-    #for file_name in file_list:
-        #os.rename(file_name, file_name.translate(None, "0123456789"))
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None, "0123456789"))
+    os.chdir(saved_path)
         # NOTE Error given. Program is not finding dir
         # DEBUG Result: Dir is a level down from the dir wanted. It's running in the same dir as the program
+        # DEBUG DONE, Program works
 rename_files()
