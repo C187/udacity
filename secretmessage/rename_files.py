62ed28e22e805a53f80b2d0809dd27b386e1ab85
Chris <c187.org@gmail.com>
2017-11-03 13:27:26 -0700

    .

--- secretmessage/rename_files.py
+++ secretmessage/rename_files.py
@@ -1,3 +1,4 @@
+import os
 def rename_files():
     # (1) get file names from a folder
     # NOTE Filed directory unique to my Win10 machine 
@@ -5,5 +6,8 @@ def rename_files():
     print(file_list)
     
     # (2) for each file, rename filename
-
+    # Udacity Lesson 5, Part 22 Quiz: Find the name of the fuction to rename. That fuction is os.rename
+    for file_name in file_list:
+        os.rename(file_name,
+                  
 rename_files()
