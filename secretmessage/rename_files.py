83125bb542aa1c2c60a784e73c47efe34917d1ea
Chris <c187.org@gmail.com>
2017-11-03 13:19:25 -0700

    .

--- secretmessage/rename_files.py
+++ secretmessage/rename_files.py
@@ -1,6 +1,8 @@
 def rename_files():
     # (1) get file names from a folder
-    os.listdir("C:\Users\c187r\OneDrive\Documents\GitHub\udacity\secretmessage\prank")
+    # NOTE Filed directory unique to my Win10 machine 
+    file_list = os.listdir(r"C:\Users\c187r\OneDrive\Documents\GitHub\udacity\secretmessage\prank")
+    print(file_list)
     
     # (2) for each file, rename filename
 
