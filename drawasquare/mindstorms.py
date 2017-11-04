ec053b38edf5cb2ef9ca8894a225a496d76c94dd
Chris <c187.org@gmail.com>
2017-11-04 16:15:59 -0700

    three shapes

--- drawasquare/mindstorms.py
+++ drawasquare/mindstorms.py
@@ -1,29 +1,38 @@
 import turtle
 
-def draw_square():
-    window = turtle.Screen()
-    window.bgcolor("black")
-    
+window = turtle.Screen()
+window.bgcolor("black")
+
+def draw_square():  
     brad = turtle.Turtle()
     brad.shape("turtle")
     brad.color("white")
     brad.speed(4)
-    
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
 
+    count = 0
+    while count < 4:
+        brad.forward(100)
+        brad.right(90)
+        count = count + 1
+    
+def draw_circle():
     angie = turtle.Turtle()
     angie.shape("arrow")
     angie.color("blue")
     angie.circle(100)
 
-    window.exitonclick()
-    
+def draw_triangle():
+    bob = turtle.Turtle()
+    bob.shape("triangle")
+    bob.color("red")
 
+    count = 0
+    while count < 3:
+        bob.forward(100)
+        bob.right(120)
+        count = count + 1
+    
+    window.exitonclick()
 draw_square()
+draw_circle()
+draw_triangle()
