924c0a7efcb064bcfcd3314ace995a64da725554
Chris <c187.org@gmail.com>
2017-11-04 16:01:03 -0700

    .

--- drawasquare/mindstorms.py
+++ drawasquare/mindstorms.py
@@ -2,9 +2,13 @@ import turtle
 
 def draw_square():
     window = turtle.Screen()
-    window.bgcolor("red")
+    window.bgcolor("black")
     
     brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("white")
+    brad.speed(4)
+    
     brad.forward(100)
     brad.right(90)
     brad.forward(100)
