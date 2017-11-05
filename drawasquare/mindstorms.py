135c888ee5a8ebc05bb2805251324050736556d3
Chris <c187.org@gmail.com>
2017-11-04 19:07:08 -0700

    Made a circle out of squares.
    woot?

--- drawasquare/mindstorms.py
+++ drawasquare/mindstorms.py
@@ -1,19 +1,28 @@
 import turtle
 
-window = turtle.Screen()
-window.bgcolor("black")
+def draw_square(some_turtle):
+    for i in range(1,5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
 
-def draw_square():  
+def draw_art():
+    window = turtle.Screen()
+    window.bgcolor("black")
+    #Create the turtle
     brad = turtle.Turtle()
     brad.shape("turtle")
     brad.color("white")
-    brad.speed(4)
+    brad.speed(1000)
+    for i in range(1,37):
+        draw_square(brad)
+        brad.right(10)
 
-    count = 0
-    while count < 4:
-        brad.forward(100)
-        brad.right(90)
-        count = count + 1
+    window.exitonclick()
+    #count = 0
+    #while count < 4:
+        #brad.forward(100)
+        #brad.right(90)
+        #count = count + 1
 
 #Below hidden to make a circle out of squares.
     
@@ -34,8 +43,9 @@ def draw_square():
         #bob.right(120)
         #count = count + 1
     
-    window.exitonclick()
     
-draw_square()
+
+draw_art()
+#draw_square()
 #draw_circle()
 #draw_triangle()
