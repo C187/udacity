7a88ec1fe16a4df6f626ae1e188ef4e77cfd6dd5
Chris <c187.org@gmail.com>
2017-11-04 18:56:40 -0700

    Making a circle out of squares start

--- drawasquare/mindstorms.py
+++ drawasquare/mindstorms.py
@@ -14,25 +14,28 @@ def draw_square():
         brad.forward(100)
         brad.right(90)
         count = count + 1
+
+#Below hidden to make a circle out of squares.
     
-def draw_circle():
-    angie = turtle.Turtle()
-    angie.shape("arrow")
-    angie.color("blue")
-    angie.circle(100)
+#def draw_circle():
+    #angie = turtle.Turtle()
+    #angie.shape("arrow")
+    #angie.color("blue")
+    #angie.circle(100)
 
-def draw_triangle():
-    bob = turtle.Turtle()
-    bob.shape("triangle")
-    bob.color("red")
+#def draw_triangle():
+    #bob = turtle.Turtle()
+    #bob.shape("triangle")
+    #bob.color("red")
 
-    count = 0
-    while count < 3:
-        bob.forward(100)
-        bob.right(120)
-        count = count + 1
+    #count = 0
+    #while count < 3:
+        #bob.forward(100)
+        #bob.right(120)
+        #count = count + 1
     
     window.exitonclick()
+    
 draw_square()
-draw_circle()
-draw_triangle()
+#draw_circle()
+#draw_triangle()
