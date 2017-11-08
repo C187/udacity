8f65009d2fdd99a5d84a2810b348bddff04b70ed
Chris <c187.org@gmail.com>
2017-11-08 14:17:26 -0800

    .

--- profanityeditor/check_profanity.py
+++ profanityeditor/check_profanity.py
@@ -3,4 +3,6 @@ def read_text():
     quotes = open("C:\Users\c187r\OneDrive\Documents\GitHub\udacity\profanityeditor\movie_quotes.txt")
     contents_of_file = quotes.read()
     print(contents_of_file)
+    quotes.close()
+    
 read_text()
