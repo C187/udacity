6def31ee1eaf34e6361ba93b9ccc983a5aebfa52
Chris <c187.org@gmail.com>
2017-11-08 14:07:32 -0800

    .

--- profanityeditor/check_profanity.py
+++ profanityeditor/check_profanity.py
@@ -1,3 +1,6 @@
 def read_text():
-
+    #NOTE path is local to my machine
+    quotes = open("C:\Users\c187r\OneDrive\Documents\GitHub\udacity\profanityeditor\movie_quotes.txt")
+    contents_of_file = quotes.read()
+    print(contents_of_file)
 read_text()
