63b50d53ec01317df24fd5a05414b71909216623
Chris <c187.org@gmail.com>
2017-11-10 12:42:13 -0800

    finds the words...

--- profanityeditor/check_profanity.py
+++ profanityeditor/check_profanity.py
@@ -1,9 +1,12 @@
+import urllib
+
 def read_text():
     #NOTE path is local to my machine
     quotes = open("C:\Users\c187r\OneDrive\Documents\GitHub\udacity\profanityeditor\movie_quotes.txt")
     contents_of_file = quotes.read()
     print(contents_of_file)
     quotes.close()
+    check_profanity(contents_of_file)
 
 def check_profanity(text_to_check):
     connection = urllib.urlopen(" http://www.wdylike.appspot.com/?q="+text_to_check)
