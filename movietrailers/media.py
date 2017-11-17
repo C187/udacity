863bd43b174d4b11bded763af28f4ff87974d691
Chris <c187.org@gmail.com>
2017-11-17 09:54:25 -0800

    fixed bug

--- movietrailers/media.py
+++ movietrailers/media.py
@@ -3,4 +3,4 @@ class Movie():
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
-        self.trailer.youtube_url = trailer_youtube
+        self.trailer_youtube_url = trailer_youtube
