07afe0689d6fa292bce8621271200baef4fe5a97
Chris <c187.org@gmail.com>
2017-11-26 10:36:27 -0800

    more movies

--- movietrailers/entertainment_center.py
+++ movietrailers/entertainment_center.py
@@ -10,5 +10,21 @@ avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
-print(avatar.storyline)
-avatar.show_trailer()
+#print(avatar.storyline)
+#avatar.show_trailer()
+
+school_of_rock = media.Movie("School of Rock", "Using rock music to learn",
+                             "http://upload.wikimedia.org/wikipedia/en/1/11/Scholl_of_Rock_Poster.jpg",
+                             "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+ratatouille = media.Movie("Ratatouille", "A rat is a chef in Paris",
+                          "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                          "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+midnight_in_paris = media.Movie("Midnight in Paris", "Going back in time to meet authors",
+                                "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+                                "https://www.youtube.com/watch?v=FAfR8omt-CY")
+
+huger_games = media.Movie("Hunger Games", "A really real reality show",
+                          "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
+                          "https://www.youtube.com/watch?v=4S9a5V9ODuY")
